7301a9056928a34f1adc1e3bb094906253df7642
alexanderzichacek <zichacekjunior@gmail.com>
2024-06-05 14:51:40 +0200

    server edit

--- server.py
+++ server.py
@@ -10,24 +10,21 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=None)
 thread = None
 thread_lock = Lock()
-sensor_thread = None
-sensor_thread_lock = Lock()
 sensor_enabled = False
+sensor_thread = None
 
 def background_thread():
+    global sensor_enabled
     # Define DHT sensor type and pin
     sensor = Adafruit_DHT.DHT11
     pin = 21  # Adjust pin number according to your setup
 
     while True:
-        with sensor_thread_lock:
-            if not sensor_enabled:
-                break
-
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-        if humidity is not None and temperature is not None:
-            # If data is valid, emit it to clients
-            socketio.emit('sensor_data', {'temperature': temperature, 'humidity': humidity}, namespace='/test')
+        if sensor_enabled:
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            if humidity is not None and temperature is not None:
+                # If data is valid, emit it to clients
+                socketio.emit('sensor_data', {'temperature': temperature, 'humidity': humidity}, namespace='/test')
         socketio.sleep(2)
 
 @app.route('/')
@@ -52,15 +49,16 @@ def test_connect():
 
 @socketio.on('start_sensor', namespace='/test')
 def start_sensor():
-    global sensor_enabled
-    with sensor_thread_lock:
+    global sensor_enabled, sensor_thread
+    if not sensor_enabled:
         sensor_enabled = True
+        sensor_thread = socketio.start_background_task(target=background_thread)
 
 @socketio.on('stop_sensor', namespace='/test')
 def stop_sensor():
-    global sensor_enabled
-    with sensor_thread_lock:
-        sensor_enabled = False
+    global sensor_enabled, sensor_thread
+    sensor_enabled = False
+    sensor_thread = None
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
