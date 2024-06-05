1a7e516027d42d5f713c7df5af88a3558cb5e485
alexanderzichacek <zichacekjunior@gmail.com>
2024-06-05 14:38:43 +0200

    add log print

--- server.py
+++ server.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template, redirect, url_for
+from threading import Lock
+from flask import Flask, render_template
 from flask_socketio import SocketIO
+
+# Import Adafruit DHT library
+import Adafruit_DHT
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode=None)
+thread = None
+thread_lock = Lock()
+
+def background_thread():
+    # Define DHT sensor type and pin
+    sensor = Adafruit_DHT.DHT11
+    pin = 4
+
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        if humidity is not None and temperature is not None:
+            # If data is valid, emit it to clients
+            socketio.emit('sensor_data', {'temperature': temperature, 'humidity': humidity}, namespace='/test')
+        socketio.sleep(2)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/numeric')
-def numeric():
-    return render_template('numeric.html')
-
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            # Start background thread to read sensor data
+            thread = socketio.start_background_task(target=background_thread)
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
